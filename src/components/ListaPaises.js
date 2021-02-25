68d6663ba1983899ce091dec67d1d0b70dee64bb
Felix-Medrano <sckrool@gmail.com>
2021-02-25 00:26:49 -0600

    Prueba 2 Pull Request

--- src/components/ListaPaises.js
+++ src/components/ListaPaises.js
@@ -26,6 +26,7 @@ const ListaPaises = () => {
           payload: lista,
         });
         // setListaPaises(data);
+        // prueba
       })
       .catch(() => {
         console.log("Falla");
