79696f5446ae53647c07c71d63d6305eb2ac7341
Felix-Medrano <sckrool@gmail.com>
2021-02-25 00:19:33 -0600

    Probando el Pull Request en VSCode

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function reductor(estado, accion) {
   // console.log(accion)
   switch (accion.type) {
     case "SET_LISTA_PAISES": {
-      return { ...estado, listaPaises: accion.payload };
+      return { estado, listaPaises: accion.payload };
+      // return { ...estado, listaPaises: accion.payload };
     }
     default: {
       return estado;
